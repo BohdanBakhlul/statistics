e0d7f467ecb9e876d9c8ea2b7f5b4d5343260183
BohdanBakhlul <43843892+BohdanBakhlul@users.noreply.github.com>
2019-10-19 00:59:23 +0200

    Add files via upload

--- intro_to_R_tasks.R
+++ intro_to_R_tasks.R
@@ -58,4 +58,39 @@ quantile(n, 1)
 #10
 library(schoolmath)
 data("primlist")
-primlist
+ab <- primlist<1000
+max(primlist[ab])
+ac <- primlist>100 & primlist<500
+length(primlist[ac])
+
+#11
+vec1 <- c("a", "b")
+vec2 <- c(1,2,3)
+paste(rep(vec1, each = length(vec2)), vec2, sep="")
+
+#12
+vec3 <- 1:30
+vec4 <- c("X", "Y", "Z")
+vec5 <- paste(vec3, vec4, sep=".")
+
+#13
+install.packages("carData")
+install.packages("car")
+library(car)
+library(carData)
+ae <- c('a', 'b','c','d', 'e')
+af <- sample(ae, 100, replace= TRUE)
+recode(af, "c('a','b') = 1; c('c','d') = 2; 'e' = 3")
+
+#14
+xx <- c(NA, 3, 14, NA, 33, 17, NA, 41)
+sum(is.na(xx))
+mean(xx, na.rm = TRUE)
+xb <- xx[!is.na(xx)]
+xb
+xx[is.na(xx)] <- 11
+xx
+
+#15
+(myList <- list(c("Bohdan", "Bakhlul"), 3.14, unique, seq(0, 1, 0.1)))
+myList
